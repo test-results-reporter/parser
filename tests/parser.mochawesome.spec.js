aa8e241989d7ddf6a4747b09e34fc6cef6311da1
Joaquín Muleiro <joaquin.muleirobeltran@wearepsh.com>
2024-05-09 12:14:11 -0300

    update tests to consider mocha & mochawesome parsers

--- tests/parser.mochawesome.spec.js
+++ tests/parser.mochawesome.spec.js
@@ -2,12 +2,11 @@ const { parse } = require('../src');
 const assert = require('assert');
 const path = require('path');
 
-describe('Parser - Mocha Json', () => {
-  
-  const testDataPath = "tests/data/mocha/json"
-  
+describe('Parser - Mocha Awesome Json', () => {
+  const testDataPath = "tests/data/mocha/awesome";
+
   it('single suite with single test', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/single-suite-single-test.json`] });
+    const result = parse({ type: 'mochawesome', files: [`${testDataPath}/single-suite-single-test.json`] });
     assert.deepEqual(result, {
       id: '',
       name: '',
@@ -53,9 +52,9 @@ describe('Parser - Mocha Json', () => {
       ]
     });
   });
-  
+
   it('empty suite report', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/empty-suite.json`] });
+    const result = parse({ type: 'mochawesome', files: [`${testDataPath}/empty-suite.json`] });
     assert.deepEqual(result, {
       id: '',
       name: '',
@@ -70,9 +69,9 @@ describe('Parser - Mocha Json', () => {
       suites: []
     });
   });
-
+  
   it('suite with skipped tests', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/skipped-tests.json`] });
+    const result = parse({ type: 'mochawesome', files: [`${testDataPath}/skipped-tests.json`] });
     assert.deepEqual(result, {
       id: '',
       name: '',
@@ -134,8 +133,103 @@ describe('Parser - Mocha Json', () => {
       ]
     });
   });
+
   it('multiple suites', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/multiple-suites-multiple-tests.json`] });
+    const result = parse({ type: 'mochawesome', files: [`${testDataPath}/multiple-suites-multiple-tests.json`] });
+    assert.deepEqual(result, {
+      id: '',
+      name: '',
+      total: 3,
+      passed: 2,
+      failed: 1,
+      errors: 0,
+      skipped: 0,
+      retried: 0,
+      duration: 7,
+      status: 'FAIL',
+      suites: [
+        {
+          id: '',
+          name: 'Example Suite 1',
+          total: 2,
+          passed: 2,
+          failed: 0,
+          errors: 0,
+          skipped: 0,
+          duration: 4,
+          status: 'PASS',
+          meta_data: new Map(),
+          cases: [
+            {
+              attachments: [],
+              duration: 3,
+              errors: 0,
+              failed: 0,
+              failure: "",
+              id: "",
+              name: "sample test case",
+              passed: 0,
+              skipped: 0,
+              stack_trace: "",
+              status: "PASS",
+              meta_data: new Map(),
+              steps: [],
+              total: 0
+            },
+            {
+              attachments: [],
+              duration: 1,
+              errors: 0,
+              failed: 0,
+              failure: "",
+              id: "",
+              name: "sample test case 2",
+              passed: 0,
+              skipped: 0,
+              stack_trace: "",
+              status: "PASS",
+              meta_data: new Map(),
+              steps: [],
+              total: 0
+            }
+          ]
+        },
+        {
+          id: '',
+          name: 'Example Suite 2',
+          total: 1,
+          passed: 0,
+          failed: 1,
+          errors: 0,
+          skipped: 0,
+          duration: 1,
+          status: 'FAIL',
+          meta_data: new Map(),
+          cases: [
+            {
+              attachments: [],
+              duration: 1,
+              errors: 0,
+              failed: 0,
+              failure: "Dummy reason",
+              id: "",
+              name: "sample test case",
+              passed: 0,
+              skipped: 0,
+              stack_trace: "",
+              status: "FAIL",
+              meta_data: new Map(),
+              steps: [],
+              total: 0
+            }
+          ]
+        }
+      ]
+    });
+  });
+
+  it('nested suites', () => {
+    const result = parse({ type: 'mochawesome', files: [`${testDataPath}/nested-suites.json`] });
     assert.deepEqual(result, {
       id: '',
       name: '',
@@ -231,36 +325,10 @@ describe('Parser - Mocha Json', () => {
   it('can support absolute and relative file paths', () => {
     let relativePath = `${testDataPath}/single-suite-single-test.json`;
     let absolutePath = path.resolve(relativePath);
-    const result1 = parse({ type: 'mocha', files: [absolutePath] });
+    const result1 = parse({ type: 'mochawesome', files: [absolutePath] });
     assert.notEqual(null, result1);
-    const result2 = parse({ type: 'mocha', files: [relativePath]});
+    const result2 = parse({ type: 'mochawesome', files: [relativePath]});
     assert.notEqual(null, result2);
   });
 
-  it('has multiple tags', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/multiple-suites-multiple-tests-tags.json`] });
-    let test_suite = result.suites[0];
-    let test_case = result.suites[0].cases[0];
-    assert.equal(test_suite.meta_data.has("tags"), true);
-    assert.equal(test_suite.meta_data.get("tags"), "");
-    assert.equal(test_suite.meta_data.get("tagsRaw"), "");
-    assert.equal(test_suite.meta_data.get("type"), "api");
-    assert.equal(test_case.meta_data.has("tags"), true);
-    assert.equal(test_case.meta_data.get("tags"), "fast,1255");
-    assert.equal(test_case.meta_data.get("tagsRaw"), "@fast,#1255");
-  });
-
-  it('has single tag', () => {
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/multiple-suites-multiple-tests-tags.json`] });
-    let test_case = result.suites[1].cases[0];
-    assert.equal(test_case.meta_data.has("tags"), true);
-    assert.equal(test_case.meta_data.get("tags"), "1234");
-    assert.equal(test_case.meta_data.get("tagsRaw"), "#1234");
-  });
-
-  it('does not include tags meta if no tags are present', () =>{
-    const result = parse({ type: 'mocha', files: [`${testDataPath}/multiple-suites-multiple-tests-tags.json`] });
-    let testcase = result.suites[0].cases[1];
-    assert.equal(testcase.meta_data.has("tags"), false);
-  });
 });
