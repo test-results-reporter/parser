af9fb392f88b02f3257e00438b0244437449b683
Anudeep <asa.anudeep4@gmail.com>
2024-07-28 08:48:11 +0530

    chore: cucumber refactor and capture steps (#77)
    * chore: cucumber refactor and capture steps
    
    * fix: duration

---
+++ src/parsers/base.parser.js
@@ -0,0 +1,48 @@
+const { unescape } = require('html-escaper');
+
+
+class BaseParser {
+
+  /**
+   * @param {string} value
+   * @returns
+   */
+  parseText(value) {
+    return value ? unescape(value) : value;
+  }
+
+  /**
+   *
+   * @param {string[]} parent_tags
+   * @param {string[]} child_tags
+   */
+  mergeTags(parent_tags, child_tags) {
+    if (!parent_tags) {
+      parent_tags = [];
+    }
+    if (!child_tags) {
+      child_tags = [];
+    }
+    for (const tag of parent_tags) {
+      if (child_tags.indexOf(tag) === -1) {
+        child_tags.push(tag);
+      }
+    }
+  }
+
+  mergeMetadata(parent_metadata, child_metadata) {
+    if (!parent_metadata) {
+      parent_metadata = {};
+    }
+    if (!child_metadata) {
+      child_metadata = {};
+    }
+    for (const [key, value] of Object.entries(parent_metadata)) {
+      if (!child_metadata[key]) {
+        child_metadata[key] = value;
+      }
+    }
+  }
+}
+
+module.exports = { BaseParser }
